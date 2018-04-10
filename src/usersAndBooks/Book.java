eec81dc6df03efa179e73fc3c741231ed7b3c88b
Bartosz Suchocki <suchar1270@wp.pl>
2018-04-10 20:15:06 +0200

    Nowe pola i konstruktory dla klasy Book

--- src/usersAndBooks/Book.java
+++ src/usersAndBooks/Book.java
@@ -1,10 +1,48 @@
 package usersAndBooks;
+
+import java.util.Date;
+
 public class Book {
 
 	private String title;
 	private String author;
 	private String signature;
 	private boolean lent;
+	private String pesel;
+	private Date date;
+	public Book(String title, String author, String signature, String pesel,Date date)
+	{
+		this.title=title;
+		this.author=author;
+		this.signature=signature;
+		this.pesel=pesel;
+		if(pesel==null)
+			this.lent=false;
+		else
+			this.lent=true;
+		this.date = date;
+	}
+	public Book(String title, String author, String signature,boolean lent, String pesel,Date date)
+	{
+		this(title,author,signature,pesel,date);
+		this.lent=lent;
+	}
+
+	public String getPesel() {
+		return pesel;
+	}
+
+	public void setPesel(String pesel) {
+		this.pesel = pesel;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
 
 	public String getTitle() {
 		return title;
