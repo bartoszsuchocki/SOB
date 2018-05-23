2143360dc2d3b1c28352a973815505ca2694008c
Mateusz <Mateusz@192.168.0.73>
2018-05-23 20:03:02 +0200

    Dodaję testy dotyczące dodawania książek do bazy.

--- src/test/DataBaseTest.java
+++ src/test/DataBaseTest.java
@@ -2,7 +2,6 @@ package test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.fail;
 
 import java.util.Date;
 import java.util.List;
@@ -17,6 +16,7 @@ import usersAndBooks.Book;
 public class DataBaseTest {
 
 	private DataBase dataBase;
+
 	@Before
 	public void setUp() throws Exception {
 	}
@@ -24,181 +24,184 @@ public class DataBaseTest {
 	@After
 	public void tearDown() throws Exception {
 	}
-	
+
 	@Before
 	public void initDatabaseObject() {
-		dataBase = new DataBase("jdbc:mysql://localhost:3306/biblioteka_testowa","user1","userpassword");
+		dataBase = new DataBase("jdbc:mysql://localhost:3306/biblioteka_testowa", "user1", "userpassword");
 	}
+
 	@After
 	public void closeDatabaseResources() {
-		
+
 	}
 
 	@Test
 	public void testGetNewBooks() {
 		List<Book> resultList;
 		Book book;
-		
+
 		resultList = dataBase.getNewBooks();
-		assertEquals(2,resultList.size());
-		
+		assertEquals(2, resultList.size());
+
 		book = resultList.get(0);
 		assertEquals("Antygona", book.getTitle());
-		assertEquals("Sofokles", book.getAuthor());	
-		
+		assertEquals("Sofokles", book.getAuthor());
+
 		book = resultList.get(1);
 		assertEquals("Potop", book.getTitle());
 		assertEquals("Henryk Sienkiewicz", book.getAuthor());
-		
+
 	}
 
 	@Test
 	public void testGetBooks() {
 		List<Book> resultList;
 		Book book;
-		
-		resultList = dataBase.getBooks("Antygona");		
-		assertEquals(1,resultList.size());
 
-		
+		resultList = dataBase.getBooks("Antygona");
+		assertEquals(1, resultList.size());
+
 		book = resultList.get(0);
-		assertEquals("Antygona",book.getTitle());
-		
-		
+		assertEquals("Antygona", book.getTitle());
+
 		resultList = dataBase.getBooks("Nie ma takiej ksi��ki");
 		assertEquals(0, resultList.size());
-		
+
 	}
 
 	@Test
 	public void testGetAllBooks() {
 		List<Book> resultList;
 		Book book;
-		
+
 		resultList = dataBase.getAllBooks();
-		assertEquals(3,resultList.size());
-		
+		assertEquals(3, resultList.size());
+
 		book = resultList.get(0);
 		assertEquals("Illiada", book.getTitle());
 		assertEquals("Homer", book.getAuthor());
-				
+
 		book = resultList.get(1);
 		assertEquals("Antygona", book.getTitle());
 		assertEquals("Sofokles", book.getAuthor());
-		
+
 		book = resultList.get(2);
 		assertEquals("Potop", book.getTitle());
 		assertEquals("Henryk Sienkiewicz", book.getAuthor());
-		
+
 	}
 
-	
-	
 	@Test
-	public void testDeleteBook() 
-	{	
-		String signature="S002";
-	
+	public void testDeleteBook() {
+		String signature = "S002";
+
 		assertEquals(1, dataBase.deleteBook(signature));
-		List<Book> potopBooks=dataBase.getBooks("Potop");
-		for(Book book: potopBooks)
-		
+		List<Book> potopBooks = dataBase.getBooks("Potop");
+		for (Book book : potopBooks)
+
 			assertFalse(book.getSignature().equals(signature));
-		
-	}
-	
-	
-	/*
-	 * Po testowaniu usunięcia z bazy chcemy z powrotem dodać
-	 * "Potop". Jeżeli już jest, to się nie doda więc w każdym razie będzie ok
-	 * */
-	
-	@After
-	public void addPotop()
-	{
-		String signature="S002";
-		String author="Henryk Sienkiewicz";
-		String title="Potop";
-		
-		Book book=new Book(title, author, signature, false, null, new Date());
-		dataBase.addBook(book);
-		
-		
+
 	}
 
-	
+	/*
+	 * Po testowaniu usunięcia z bazy chcemy z powrotem dodać "Potop". Jeżeli
+	 * już jest, to się nie doda więc w każdym razie będzie ok
+	 */
+
+	@After
+	public void addPotop() {
+		String signature = "S002";
+		String author = "Henryk Sienkiewicz";
+		String title = "Potop";
+
+		Book book = new Book(title, author, signature, false, null, new Date());
+		dataBase.addBook(book);
+
+	}
 
 	@Test
-	public void testReturnBook() 
-	{
-		
+	public void testReturnBook() {
+
 		assertEquals(1, dataBase.returnBook("H001"));
-		List<Book> illiadaBooks=dataBase.getBooks("Illiada");
-		for(Book book: illiadaBooks)
-		{
-			if(book.getSignature().equals("H001")) assertFalse(book.isLent());
+		List<Book> illiadaBooks = dataBase.getBooks("Illiada");
+		for (Book book : illiadaBooks) {
+			if (book.getSignature().equals("H001"))
+				assertFalse(book.isLent());
 		}
 	}
 
-
-	
 	/*
-	 * Zmieniamy z powrotem status Illiady
-	 * po testowaniu returnBook();
-	 * */
-	
-	@After 
-	public void lendIlliada()
-	{
-		String signature="H001";
-		String userName="janko";
-		
+	 * Zmieniamy z powrotem status Illiady po testowaniu returnBook();
+	 */
+
+	@After
+	public void lendIlliada() {
+		String signature = "H001";
+		String userName = "janko";
+
 		dataBase.lendBook(signature, userName);
 	}
-	
 
-
-	/*
 	@Test
 	public void testAddBookStringStringString() {
-		fail("Not yet implemented");
+		List<Book> resultList;
+
+		assertEquals(1, dataBase.addBook("G001", "Ferdydurke", "Witold Gombrowicz"));
+
+		resultList = dataBase.getAllBooks();
+
+		assertEquals(4, resultList.size());
+		assertEquals("G001", resultList.get(0).getSignature());
+		assertEquals("Ferdydurke", resultList.get(0).getTitle());
+		assertEquals("Witold Gombrowicz", resultList.get(0).getAuthor());
+	}
+
+	@After
+	public void deleteFerdydurke() {
+		dataBase.deleteBook("G001");
 	}
 
 	@Test
 	public void testAddBookBook() {
-		fail("Not yet implemented");
+		List<Book> resultList;
+		Book book = new Book("Faust", "Goethe", "G002", null, new Date(5));
+
+		assertEquals(1, dataBase.addBook(book));
+
+		resultList = dataBase.getAllBooks();
+
+		assertEquals("G002", resultList.get(0).getSignature());
+		assertEquals("Faust", resultList.get(0).getTitle());
+		assertEquals("Goethe", resultList.get(0).getAuthor());
 	}
 
-	@Test
-	public void testLendBook() {
-		fail("Not yet implemented");
+	@After
+	public void deleteFaust() {
+		dataBase.deleteBook("G002");
 	}
 
-	
-
-	@Test
-	public void testGetUser() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testAddUser() {
-		fail("Not yet implemented");
-	}
-
-*/
+	/*
+	 * @Test public void testLendBook() { fail("Not yet implemented"); }
+	 * 
+	 * 
+	 * 
+	 * @Test public void testGetUser() { fail("Not yet implemented"); }
+	 * 
+	 * @Test public void testAddUser() { fail("Not yet implemented"); }
+	 * 
+	 */
 	@Test
 	public void testGetUserBooks() {
 		List<Book> resultList;
-		
+
 		resultList = dataBase.getUserBooks("janko");
-		
+
 		assertEquals(1, resultList.size());
 		assertEquals("Illiada", resultList.get(0).getTitle());
-		
+
 		resultList = dataBase.getUserBooks("kowal");
-		assertEquals(0,resultList.size());
-		
+		assertEquals(0, resultList.size());
+
 	}
 
 }
